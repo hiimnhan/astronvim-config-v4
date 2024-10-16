6dade0101eed9c54dc7b47167af5edd21980ce8c
nhanncv <vinhnhan1999bc@gmail.com>
2024-10-16 22:21:25 +0700

    use skim for latex

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -1,7 +1,7 @@
 return {
   "lervag/vimtex",
   lazy = false,
-  init = function() vim.g.vimtex_view_method = "sioyek" end,
+  init = function() vim.g.vimtex_view_method = "skim" end,
   dependencies = {
     "AstroNvim/astrocore",
     opts = {
