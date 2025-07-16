c0717868a25f8214717727348664e4dc77dcc838
nhanncv <vinhnhan1999bc@gmail.com>
2025-07-16 10:14:39 +0700

    update typst

--- lua/plugins/tinymist.lua
+++ lua/plugins/tinymist.lua
@@ -4,8 +4,38 @@ return {
     servers = { "tinymist" },
     config = {
       tinymist = {
-        settings = {},
-        root_dir = function(filename, bufnr) return vim.fn.getcwd() end,
+        settings = {
+          projectResolution = "lockDatabase",
+          semanticTokens = "enable",
+        },
+        root_dir = function() return vim.fn.getcwd() end,
+        on_attach = function(client, bufnr)
+          vim.keymap.set(
+            "n",
+            "<leader>tm",
+            function()
+              client:exec_cmd({
+                title = "pin",
+                command = "tinymist.pinMain",
+                arguments = { vim.api.nvim_buf_get_name(0) },
+              }, { bufnr = bufnr })
+            end,
+            { desc = "[T]inymist [P]in", noremap = true }
+          )
+
+          vim.keymap.set(
+            "n",
+            "<leader>tM",
+            function()
+              client:exec_cmd({
+                title = "unpin",
+                command = "tinymist.pinMain",
+                arguments = { vim.v.null },
+              }, { bufnr = bufnr })
+            end,
+            { desc = "[T]inymist [U]npin", noremap = true }
+          )
+        end,
       },
     },
   },
