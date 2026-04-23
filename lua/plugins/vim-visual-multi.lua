6ba60ea57fa4e7e5b156a6db97d15cd9faef9899
nhanncv <vinhnhan1999bc@gmail.com>
2026-04-24 00:14:22 +0100

    update vim-visual-multi + todo-comments

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -1,3 +1,19 @@
 return {
   "mg979/vim-visual-multi",
+  branch = "master",
+  event = "VeryLazy",
+  init = function()
+    vim.g.VM_maps = {
+      ["Find Under"] = "<C-n>",
+      ["Find Subword Under"] = "<C-n>",
+      ["Add Cursor Down"] = "<C-j>",
+      ["Add Cursor Up"] = "<C-k>",
+      ["Select All"] = "<C-A-n>",
+      ["Skip Region"] = "q",
+      ["Remove Region"] = "Q",
+      ["Start Regex Search"] = "\\/",
+    }
+    vim.g.VM_highlight_matches = "underline"
+    vim.g.VM_theme = "iceblue"
+  end,
 }
