c2a602c1c07f363f21bd2816506318a4ddf35ff0
nhanncv <vinhnhan1999bc@gmail.com>
2026-05-12 19:07:36 +0100

    update typst preview

--- lua/plugins/typst-preview.lua
+++ lua/plugins/typst-preview.lua
@@ -1,10 +1,17 @@
 return {
   {
     "chomosuke/typst-preview.nvim",
-    lazy = false, -- or ft = 'typst'
+    lazy = false,
     version = "1.*",
     opts = {
       open_cmd = "qutebrowser %s",
-    }, -- lazy.nvim will implicitly calls `setup {}`
+    },
+    keys = {
+      { "<leader>tp", "<cmd>TypstPreview<cr>",             desc = "Typst Preview Open" },
+      { "<leader>tP", "<cmd>TypstPreviewStop<cr>",         desc = "Typst Preview Stop" },
+      { "<leader>tt", "<cmd>TypstPreviewToggle<cr>",       desc = "Typst Preview Toggle" },
+      { "<leader>tf", "<cmd>TypstPreviewFollowCursor<cr>", desc = "Typst Follow Cursor" },
+      { "<leader>tF", "<cmd>TypstPreviewNoFollowCursor<cr>", desc = "Typst No Follow Cursor" },
+    },
   },
 }
